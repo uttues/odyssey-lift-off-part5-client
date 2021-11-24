1c02af73c74586536b971203761278259521caa6
leixin.rain <leixin.rain@bytedance.com>
2021-11-24 17:28:52 +0800

    feat(main): uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://odyssey-test-create.herokuapp.com/', // change this to YOUR server URI
   cache: new InMemoryCache(),
 });
 
